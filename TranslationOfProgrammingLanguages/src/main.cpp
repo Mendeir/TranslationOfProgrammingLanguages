ddaebad2a07c3c1f4eacdf071c1b944cce1f8c36
dbornilla <78960805+dbornilla@users.noreply.github.com>
2022-07-17 23:19:18 +0800

    Added type cheking and variable declarations

--- TranslationOfProgrammingLanguages/src/main.cpp
+++ TranslationOfProgrammingLanguages/src/main.cpp
@@ -1,14 +1,20 @@
 #include <string>
 #include "Compiler.h"
-
+#include "TypeCheck.h"
+#include <iostream>
 using namespace std;
 
 int main() 
 {
 
 	string fileName = "code.txt";
-	Compiler compiler(fileName);
-	compiler.compileCode();
+	Lexer lexer(fileName);
+	lexer.retrieveFile();
+	lexer.tokenize();
+
+	TypeCheck typeCheck;
+	typeCheck.checkTokens(lexer.getTokens());
+	cout << typeCheck.getResult();
 
 	return 0;
 }
