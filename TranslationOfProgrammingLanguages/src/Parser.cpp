35acf561ed9dfc875cc9309db330931e82b6b52a
Lorenze Dionell K. Arroyo <lorenze.arroyo@gmail.com>
2022-07-17 23:12:33 +0800

    Finalized Parser
    Finished functions for input, output and solvExpr. Dropped precedence due to time constraints.

--- TranslationOfProgrammingLanguages/src/Parser.cpp
+++ TranslationOfProgrammingLanguages/src/Parser.cpp
@@ -13,22 +13,12 @@ using namespace std;
 
 Parser::Parser(vector <Token> tokens)
 {
-	cout << "\nParsing Tokens...\n\n";
+	cout << "\nParsing Tokens...\nProgram Output:\n\n";
 	this->code = tokens;
 	this->index = 0;
 
-
 	syntaxAnalyzer();
-
-	/*
-	for (int i = 0; i < tokens.size(); i++) {
-		cout << "Type: " << tokens[i].getType() << " | Value: \"" << tokens[i].getValue() << "\"\n";//for debugging
-		syntaxAnalyzer(tokens[i], i);
-	}
-	*/
 }
-
-
 
 //***************************
 //*		   METHODS          *
@@ -36,28 +26,35 @@ Parser::Parser(vector <Token> tokens)
 void Parser::syntaxAnalyzer()
 {
 	while (true) {
-		cout << "Type: " << this->code.at(this->index).getType() << " | Value: \"" << this->code.at(this->index).getValue() << "\"\n";//for debugging
+		//cout << "Type: " << this->code.at(this->index).getType() << " | Value: \"" << this->code.at(this->index).getValue() << "\"\n";//for debugging
 		if (this->code.at(this->index).getType() == "KEYWORD") {// if current token is a keyword
 			if (this->code.at(this->index).getValue() == "var") {// VARIABLE
-				if (nextType(this->index) == "IDENTIFIER") {
-					variables[this->code.at(this->index+1).getValue()] = NULL; // create empty variable
+				if (nextType(this->index) == "VARIABLE") {
+					variables[this->code.at(this->index + 1).getValue()] = NULL; // create empty variable
 					nextToken();
 					continue;
 				}
 			}
 			else if (this->code.at(this->index).getValue() == "output") {// OUTPUT
-				if (nextType(this->index) == "IDENTIFIER") {
+				if (nextType(this->index) == "VARIABLE") {
 					//insert Master Carl's function
+					output(this->code.at(this->index + 1)); //+1 on index because current token is "output"
+					nextToken();
+					continue;
 				}
 			}
 			else {
-
+				if (nextType(this->index) == "IDENTIFIER") {
+					cout << "\nInput:";
+					cin >> variables[this->code.at(this->index + 1).getValue()];
+				}
+				nextToken();
 			}
 		}
-		else if (this->code.at(this->index).getType() == "IDENTIFIER") {// if current token is an identifier
+		else if (this->code.at(this->index).getType() == "VARIABLE") {// if current token is an VARIABLE
 			if (variables.find(this->code.at(this->index).getValue()) != variables.end()) {// check if variable exists
 				// found
-				if (nextType(this->index) == "ASSIGN_OP") {
+				if (nextType(this->index) == "ASSIGN_OPERATOR") {
 					variables[this->code.at(this->index).getValue()] = NULL; // create empty variable
 					nextToken();
 					continue;
@@ -72,17 +69,17 @@ void Parser::syntaxAnalyzer()
 			}
 			else {
 				// not found
-				cout << "Identifier (" + this->code.at(this->index).getValue() + ") NOT found...";
+				cout << "VARIABLE (" + this->code.at(this->index).getValue() + ") NOT found...";
 				breakParse();
 			}
 
 		}
-		else if(this->code.at(this->index).getType() == "ASSIGN_OP") {
+		else if (this->code.at(this->index).getType() == "ASSIGN_OPERATOR") {
 			string var_name = this->code.at(this->index - 1).getValue();
 
 			vector <Token> expression; // vector containing combination of literals and operators
 			nextToken();
-		
+
 			while (this->code.at(this->index).getType() != "TERMINATOR") {// puts tokens in the expr in the vector
 				expression.push_back(code.at(this->index));
 				nextToken();
@@ -95,22 +92,45 @@ void Parser::syntaxAnalyzer()
 			nextToken();
 		}
 	}
-	
 }
 
 int Parser::solvExpr(vector <Token> expression)
 {
-	//return 69;
-	int result = 0;
+	//example input "5 + 8 * 10 + num"
 
-	for (Token num : expression) {
-		result += stoi(num.getValue());
+	vector <Token> temp;
+	int result = stoi(expression.at(0).getValue()); //first literal
+
+	for (int i = 1; i < expression.size(); i++) {// look for */
+		//cout << expression.at(i).getValue();
+		if (expression.at(i).getType() == "LITERAL") {
+			if (nextType(i) == "OPERATOR" || i == expression.size() - 1) {
+				continue;
+			}
+			else {
+				continue;
+			}
+		}
+		else if (expression.at(i).getType() == "ARITH_OPERATOR") {
+			if (expression.at(i).getValue() == "*") {
+				result *= stoi(expression.at(i+1).getValue());
+				continue;
+			}
+			else if (expression.at(i).getValue() == "/") {
+				result /= stoi(expression.at(i+1).getValue());
+				continue;
+			}
+			else if (expression.at(i).getValue() == "+") {
+				result += stoi(expression.at(i+1).getValue());
+				continue;
+			}
+			else if (expression.at(i).getValue() == "-") {
+				result -= stoi(expression.at(i+1).getValue());
+				continue;
+			}
+		}
 	}
 	return result;
-}
-
-void Parser::checkCurrent()
-{
 }
 
 string Parser::nextType(int current_index)
@@ -118,35 +138,42 @@ string Parser::nextType(int current_index)
 	return code.at(static_cast<std::vector<Token, std::allocator<Token>>::size_type>(current_index) + 1).getType();
 }
 
-void Parser::match()
-{
-}
-
 void Parser::nextToken()
-{	
-	cout << this->code.size() << " " << this->index << " " << this->code.at(this->index).getValue() << endl;
-	if (this->index < this->code.size()-1) {
-		this->index += 1;
-	}
-	else {
+{
+	//cout << this->code.size() << " " << this->index << " " << this->code.at(this->index).getValue() << endl;
+	if (this->index >= this->code.size() - 1) {
 		endCode();
 		return;
+	}
+	else {
+		this->index += 1;
 	}
 }
 
 void Parser::breakParse()
 {
 	cout << "ERROR: ";
-	//error_message("IDENTIFIER or INT expected after" + token.getValue());
+	//error_message("VARIABLE or LITERAL expected after" + token.getValue());
 	exit(0);
 }
 
 void Parser::endCode()
 {
-	cout << "End of program reached... <3" << endl;
+	cout << "\n\n\nEnd of program reached... <3";
+	/*
 	cout << "LIST OF VARIABLES...\n";
 	for (auto it = variables.cbegin(); it != variables.cend(); ++it) {
 		cout << "{" << (*it).first << ": " << (*it).second << "}\n";
 	}
+	*/
 	exit(0);
+}
+
+void Parser::output(Token token) {
+	if (variables.find(token.getValue()) != variables.end()) {
+		cout << variables[token.getValue()];
+	}
+	else {
+		breakParse();
+	}
 }
--- TranslationOfProgrammingLanguages/src/main.cpp
+++ TranslationOfProgrammingLanguages/src/main.cpp
@@ -23,16 +23,34 @@ int main()
 	vector <Token> code;
 	//var num = 0;
 	Token token01("KEYWORD", "var");
-	Token token02("IDENTIFIER", "num");
-	Token token03("ASSIGN_OP", "=");
-	Token token04("INT", "230192");
-	Token token05("TERMINATOR", ";");
+	Token token02("VARIABLE", "num");
+	Token token03("ASSIGN_OPERATOR", "=");
+	Token token04("LITERAL", "5");
+	Token token05("ARITH_OPERATOR", "+");
+	Token token06("LITERAL", "10");
+	Token token07("ARITH_OPERATOR", "*");
+	Token token08("LITERAL", "10");
+	Token token09("ARITH_OPERATOR", "+");
+	Token token10("LITERAL", "8");
+	Token token11("TERMINATOR", ";");
+	Token token12("KEYWORD", "output");
+	Token token13("VARIABLE", "num");
+	Token token14("TERMINATOR", ";");
 
 	code.push_back(token01);
 	code.push_back(token02);
 	code.push_back(token03);
 	code.push_back(token04);
 	code.push_back(token05);
+	code.push_back(token06);
+	code.push_back(token07);
+	code.push_back(token08);
+	code.push_back(token09);
+	code.push_back(token10);
+	code.push_back(token11);
+	code.push_back(token12);
+	code.push_back(token13);
+	code.push_back(token14);
 	Parser parser01(code);
 
 	return 0;
